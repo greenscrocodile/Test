d6040f82eac087335d4dfbee11c52fd04197a513
greenscrocodile <greenscrocodile@gmail.com>
2026-05-24 11:09:41 +0530

    Update average_calculator.py

--- average_calculator.py
+++ average_calculator.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 
-def show_bill_calculator():
+def show_average_calculator():
     st.title("📊 Average Calculator Calculator")
     st.write("---")
     st.info("Work in Progress: Average Calculator features are being developed.")
